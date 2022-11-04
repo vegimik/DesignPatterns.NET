4dcbc162fa8596a9d2efbaddf32f79446a04e3fd
Vegim Karaçica <vegimi.k@hotmail.com>
2022-11-04 17:18:29 +0100

    Functional patterns, F#

---
+++ FunctionalPatternsFSharp/1_FSharp_Builder.fs
@@ -0,0 +1,16 @@
+﻿module _1_FSharp_Builder
+
+let p args =
+    let allArgs = args |> String.concat "\n"
+    ["<p>"; allArgs; "</p>"] |> String.concat "\n"
+
+let img url = "<img src=\"" + url + "\"/>"
+
+let _1_FSharp_Builder_Drive argv =
+  let html =
+    p [
+      "Check out this picture";
+      img "pokemon.com/pikachu.png"
+    ]
+  printfn "%s" html
+  0
---
+++ FunctionalPatternsFSharp/Program.fs
@@ -0,0 +1,19 @@
+﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+
+open System
+let _1_FSharp_Builder_Drive= _1_FSharp_Builder._1_FSharp_Builder_Drive
+let _2_FSharp_Decorator_Drive= _2_FSharp_Decorator.main
+
+// Define a function to construct a message to print
+let from whom =
+    sprintf "from %s" whom
+
+[<EntryPoint>]
+let main argv =
+    let message = from "F#" // Call the function
+    printfn "Hello world %s" message
+    printfn "\n"
+    _1_FSharp_Builder_Drive argv
+    printfn "\n"
+    _2_FSharp_Decorator_Drive argv
+    0 // return an integer exit code
